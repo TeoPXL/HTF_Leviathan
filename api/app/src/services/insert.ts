b711f10a5b2df994a239a1f769d9f9994ddb188f
twlv <2-twlv@users.noreply.genesis.gitlab>
2025-11-12 14:50:40 +0100

    Seeds database with Indian Chief voyage and event data.

--- api/app/src/services/insert.ts
+++ api/app/src/services/insert.ts
@@ -30,4 +30,5 @@ if (import.meta.main) {
     executeSqlFile("./src/assets/test2.sql").catch((e) => process.exit(1));
     executeSqlFile("./src/assets/test3.sql").catch((e) => process.exit(1));
     executeSqlFile("./src/assets/big.sql").catch((e) => process.exit(1));
+    executeSqlFile("./src/assets/test4.sql").catch((e) => process.exit(1));
 }
